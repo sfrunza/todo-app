37884b76de9ae953b7786e970a82cf4953ee74db
Sergiu Frunza <frunza.sergiu3@gmail.com>
2021-11-11 23:38:36 -0500

    date bugs fixing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "todo_items#index"
   resources :todo_items
   resources :todo_dates, only: [:show]
+  # resources :todo_dates
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
